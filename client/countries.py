8bfbae99e7d3ca2ee0416435196d97fb9380f11a
Yathartha Panigrahy <ypanigrahy@uwaterloo.ca>
2021-12-22 13:40:40 -0500

    minor refactor

--- client/countries.py
+++ client/countries.py
@@ -5,18 +5,17 @@ import getpass
 
 def country(options, usr, pwd, host, db):
     for i in range(len(options)): 
-        if (options[i] == "-all"):
-            getAllCountries(usr, pwd, host, db)
-        elif (options[i] == "-code"):
+        if (options[i] == "-code"):
             getCountry(usr, pwd, host, db, options[1])
+        elif (options[i] == "-all"):
+            getAllCountries(usr, pwd, host, db)
 
 
-def getAllCountries(usr, pwd, host, db):
+def getCountry(usr, pwd, host, db, code):
     cnx = mysql.connector.connect(user = usr, password = pwd, host = host, database = db)
     cursor = cnx.cursor()
 
-    query = ("SELECT * FROM countries")
-
+    query = "SELECT * FROM countries WHERE code = '{}'".format(code)
     cursor.execute(query)
     result = cursor.fetchall()
 
@@ -25,11 +24,11 @@ def getAllCountries(usr, pwd, host, db):
     cursor.close()
     cnx.close()
 
-def getCountry(usr, pwd, host, db, code):
+def getAllCountries(usr, pwd, host, db):
     cnx = mysql.connector.connect(user = usr, password = pwd, host = host, database = db)
     cursor = cnx.cursor()
 
-    query = "SELECT * FROM countries WHERE code = '{}'".format(code)
+    query = ("SELECT * FROM countries")
     cursor.execute(query)
     result = cursor.fetchall()
 
