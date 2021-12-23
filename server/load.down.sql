8dbe26c9df1a73acd377762c8ccc86ec830d7b29
sid42 <s34bhatt@uwaterloo.ca>
2021-12-22 19:42:20 -0500

    add er, rename load to server

---
+++ server/load.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS population;
+DROP TABLE IF EXISTS infant_mortality;
+DROP TABLE IF EXISTS fertility_rates;
+DROP TABLE IF EXISTS birth_death_rates;
+DROP TABLE IF EXISTS life_expectancy;
+DROP TABLE IF EXISTS gross_national_income_by_gender;
+DROP TABLE IF EXISTS human_development_index;
+DROP TABLE IF EXISTS gender_inequality_index;
+DROP TABLE IF EXISTS education;
